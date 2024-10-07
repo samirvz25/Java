814babe352cdc6bb86fdfa39310e30c8e37ebb80
samir <Samirvillanueva68@gmail.com>
2024-10-07 03:05:37 -0500

    First commit: Java Script file

---
+++ operadores.js
@@ -0,0 +1,23 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//concatenar
+console.log("Hola"+"Samir");
+var nombre = "Samir";
+var saludo = "Hola"+nombre;
+console.log(saludo);
